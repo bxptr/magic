dc49126b2a315e83061f90a8a79b678a72d1252e
Aarush Gupta <git@aarushgupta.com>
2024-01-11 22:59:16 -0800

    add tensor implementation on jax

---
+++ magic/tensor.py
@@ -0,0 +1,36 @@
+import jax.numpy as np
+
+from typing import Union
+
+class Tensor:
+    """ basic tensor """
+
+    def __init__(self, data: float) -> "Tensor":
+        self.data = np.array(data)
+
+    def __add__(self, x: Union["Tensor", float]) -> "Tensor":
+        if isinstance(x, Tensor):
+            return Tensor(self.data + x.data)
+        else:
+            return Tensor(self.data + x)
+
+    def __sub__(self, x: Union["Tensor", float]) -> "Tensor":
+        if isinstance(x, Tensor):
+            return Tensor(self.data - x.data)
+        else:
+            return Tensor(self.data - x)
+
+    def __mul__(self, x: Union["Tensor", float]) -> "Tensor":
+        if isinstance(x, Tensor):
+            return Tensor(self.data * x.data)
+        else:
+            return Tensor(self.data * x)
+
+    def __truediv__(self, x: Union["Tensor", float]) -> "Tensor":
+        if isinstance(x, Tensor):
+            return Tensor(self.data / x.data)
+        else:
+            return Tensor(self.data / x)
+
+    def __repr__(self):
+        return f"Tensor({self.data})"
