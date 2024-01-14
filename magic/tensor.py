4ea37bc97ce6c60ab72cb53db377bc512d3bf12e
Aarush Gupta <git@aarushgupta.com>
2024-01-13 22:05:22 -0800

    tensor: allow create tensor from tensor

--- magic/tensor.py
+++ magic/tensor.py
@@ -6,11 +6,15 @@ from typing import Union
 class Tensor:
     """ basic tensor """
 
-    def __init__(self, data: float) -> "Tensor":
-        self.data = jax.device_put(data)
-
-        self.grad = 0
-        self.requires_grad = False
+    def __init__(self, data: object) -> "Tensor":
+        if isinstance(data, Tensor):
+            self.data = jax.device_put(data.data)
+            self.grad = data.grad
+            self.requires_grad = data.requires_grad
+        else:
+            self.data = jax.device_put(data)
+            self.grad = 0
+            self.requires_grad = False
 
     def __add__(self, x: Union["Tensor", float]) -> "Tensor":
         if isinstance(x, Tensor):
